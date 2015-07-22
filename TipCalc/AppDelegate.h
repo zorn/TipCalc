8b4632093cffe20a455ee1c87c8e2a41e5930eaa
Mike Zornek <mike@clickablebliss.com>
2015-07-22 16:23:37 -0700

    Adding default new app.

---
+++ TipCalc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalc
+//
+//  Created by Michael D Zornek on 7/22/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
